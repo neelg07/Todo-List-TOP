e4f553667a3742e5ccf940a89e65acdf832c6656
neelg07 <ineel7762@gmail.com>
2023-04-11 22:08:04 -0400

    Update and bundle current project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import MenuBar from "./modules/MenuBar"
 /**Primary Module
  * imports other modules as needed to implement app functionality
  */
-import MenuBar from "./modules/MenuBar"
 
 // On first page load
 // Add event listeners to Menu items
