1ee62d34d9a4ab250015f6c2a79625e791c2a7c4
neelg07 <ineel7762@gmail.com>
2023-04-11 21:26:34 -0400

    Add add project DOM logic and bundle Add functions to allow switching between add project button and add project form in sidebar

--- src/modules/MenuBar.js
+++ src/modules/MenuBar.js
@@ -10,18 +10,45 @@ const week = document.querySelector('.week');
 const important = document.querySelector('.important');
 const notes = document.querySelector('.notes');
 
+// Switches between "add project div" and "add project form"
+const addProjectDiv = document.querySelector('.add-projects');
+const projectForm = document.querySelector('.project-form');
+const projectTitleInput = document.getElementById('project-title');
+const cancelProjectBtn = document.getElementById('cancel-project');
+
+function switchProjectMode() {
+    addProjectDiv.classList.toggle('hidden');
+    projectForm.classList.toggle('hidden');
+    projectTitleInput.focus();
+}
+
+function submitProjectForm(event) {
+    // TODO: logic to check constraint validation api
+    // if input is valid, continue w/ form submission
+
+    event.preventDefault();
+    switchProjectMode();
+}
+
+// Menu Bar Class 
 export default class MenuBar {
     
     static miscellaneous = [allTasks, today, week, important, notes];
 
     static addOnClicks() {
-        for (let section of MenuBar.miscellaneous) {
+        for (let section of MenuBar.miscellaneous) {    // misc section onClick events
             section.addEventListener('click', () => {
                 section.classList.add('selected');
                 MenuBar.removeOtherSelected(section);
                 MenuBar.renderPage(section);
             });
         }
+        addProjectDiv.addEventListener('click', switchProjectMode);      // Add Project section onClick events
+        cancelProjectBtn.addEventListener('click', switchProjectMode);
+        projectForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            switchProjectMode();
+        });
     }
 
     static removeOtherSelected(section) {
@@ -34,6 +61,6 @@ export default class MenuBar {
     }
 
     static renderPage(section) {
-        console.log('rendering: ', section); // implement render logic later
+        console.log('rendering: ', section); // implement render page logic later
     }
 };
