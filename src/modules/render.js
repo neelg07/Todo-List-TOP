d5f7e5e5a416af93268b3b42cc5fac95dffe0b4e
neelg07 <ineel7762@gmail.com>
2023-04-14 23:35:14 -0400

    Create render module to render each section

---
+++ src/modules/render.js
@@ -0,0 +1,12 @@
+/**Render Module
+ * is called by onclick listeners via MenuBar.renderPage method
+ * dynamically renders the page selected in the .main section
+ */
+
+class RenderPage {
+
+    static render(page) {
+        // TODO 
+        // Use if statements based on which category/section passed in as argument
+    }
+}
