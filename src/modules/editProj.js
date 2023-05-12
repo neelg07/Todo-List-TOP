b3acaebdfc61d9cbca420c076c2a8e71843714c9
neelg07 <ineel7762@gmail.com>
2023-05-12 11:22:10 -0400

    Fix project Div delete function and rendering

--- src/modules/editProj.js
+++ src/modules/editProj.js
@@ -1,3 +1,5 @@
+import MenuBar from "./MenuBar";
+
 // Clears the project div selected and calls helper function
 export function addEditProjectEvent(button, project) {
     button.addEventListener('click', () => {
@@ -28,4 +30,38 @@ function addEditProjBtns(project, projDiv) {
     projDiv.appendChild(del);
     projDiv.appendChild(cancel);
     projDiv.classList.add('edit-menu');
+
+    projDiv.childNodes.forEach(buttonNode => {          // add event listener to each button
+        addProjectEditListeners(buttonNode, project);   // variant on type of buttonNode
+    });
+}
+
+// Adds event listener to each of the different buttons generated in Project edit mode
+function addProjectEditListeners(button, project) {
+    button.addEventListener('click', () => {
+        if (button.id === 'delete-proj') {                                             // if delete btn pressed
+            const index = MenuBar.projects.findIndex((proj) => proj === project);      // find index of the project deleted in array
+            MenuBar.projects.splice(index, 1);                                         // splice project from array
+            renderPageAfterDelete();                                                   // helper func to re-render proj section
+        } else if (button.id === 'edit-proj') {
+            console.log(button);
+        } else {
+
+        }
+    })
+}
+
+// Renders a page after deleting a projDiv
+// Dependant on the length of MenuBar.projects
+// after deletion of project from array
+function renderPageAfterDelete() {
+    Promise.resolve(MenuBar.renderProjectSection()).then(() => {                        // resolve render of project section
+        if (MenuBar.projects.length >= 1) {                                             // then click on first proj in section
+            const firstProj = document.querySelector('.my-projects').childNodes[0];     // or allTasks if no projects present
+            setTimeout(() => firstProj.click(), 0);                                     // setTimeout used to prevent event loop issues
+        } else {
+            const allTasks = document.querySelector('.all-tasks');
+            setTimeout(() => allTasks.click(), 0);
+        }
+    }).catch((err) => console.log(err));
 }
