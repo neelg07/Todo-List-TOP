2540511abdcd641e25901285fa1d702c4a8be975
neelg07 <ineel7762@gmail.com>
2023-05-24 21:58:22 -0400

    Implement expand task button functions; On expandBtn click, func will check if expanding or un-expanding. Dom-rendering funcs on expand were implemented;

--- src/modules/taskEvents.js
+++ src/modules/taskEvents.js
@@ -27,9 +27,17 @@ function addExpandListener(task, expandImg) {
 
     expandImg.addEventListener('click', () => {
         const taskNode = expandImg.parentNode.parentNode.parentNode;
-        if (task.detail) taskNode.appendChild(addTaskDetail(task));
-        // addTaskProject(task);
-        // addDeleteTaskBtn(task);
+        taskNode.classList.toggle('expanded');
+
+        if (taskNode.classList.contains('expanded')) {
+            if (task.detail) taskNode.appendChild(addTaskDetail(task));
+            if (task.project !== 'none') taskNode.appendChild(addTaskProject(task));
+            taskNode.appendChild(addDeleteTaskBtn(task));    
+        }
+        else {
+            // removeExpanded(taskNode);
+        }
+        
     })
 }
 
@@ -43,6 +51,26 @@ function addTaskDetail(task) {
     return detailDiv;
 }
 
+// Adds div containing the project it's linked to if not 'none'
+function addTaskProject(task) {
+    const projectDiv = document.createElement('div');
+    projectDiv.classList.add('task-project');
+    projectDiv.append(task.project);
+
+    return projectDiv;
+}
+
+// adds dom rendered button to delete the entire task
+function addDeleteTaskBtn(task) {
+    const deleteTaskBtn = document.createElement('button');
+    deleteTaskBtn.classList.add('delete-task-btn');
+    deleteTaskBtn.append('Delete Task');
+
+    return deleteTaskBtn;
+}
+
+// add event listener to circular checkbox on left of task title
+// adds strikethrough style to task title indicating completion
 export function addCheckEventListener(check, task_note) {
     check.addEventListener('change', () => {
         if (check.checked) {
